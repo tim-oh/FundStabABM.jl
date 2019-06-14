1b1dbf15861e32f856fd75619709484c092bada6
tim_oh <tim.ohlenburg.16@ucl.ac.uk>
2019-06-14 17:55:23 +0100

    Adjusted path for runtests after moving it to test folder

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using Test
 using FundStabABM
 
 @testset "All tests" begin
-    include("test/types_test.jl")
-    include("test/params_test.jl")
-    include("test/functions_test.jl")
+    include("./types_test.jl")
+    include("./params_test.jl")
+    include("./functions_test.jl")
 #    include("tst.jl")
 end
